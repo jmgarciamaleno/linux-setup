56d6260c8a75d93003ad5a35e88c5e026506cd68
Jose Manuel García Maleno <jmgarciamaleno@gmail.com>
2020-02-05 12:43:54 +0100

    Don't use cows in Ansible output. Manage specific Ubuntu version packages.

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@
 sudo apt update
 sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install -y git ansible
+sudo apt install -y git ansible && \
+sudo sed -ir 's/^#nocows = 1/nocows = 1/' /etc/ansible/ansible.cfg
 
 # Pull and run the linux-setup project.
 sudo ansible-pull \
