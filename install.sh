6c714b10440c796aa7756f97a26a95ce332e4e1a
Jose Manuel García Maleno <jmgarciamaleno@gmail.com>
2020-02-05 12:54:27 +0100

    Even better installer feedback.

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Install Git & Ansible."
+echo -e "\nInstall Git & Ansible.\n"
 sudo apt update
 sudo apt install -y software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y git ansible && \
 sudo sed -ir 's/^#nocows = 1/nocows = 1/' /etc/ansible/ansible.cfg
 
-echo "Pull and run the linux-setup project."
+echo -e "\nPull and run the linux-setup project.\n"
 sudo ansible-pull \
     -U https://github.com/jmgarciamaleno/linux-setup.git \
     --accept-host-key \
@@ -16,4 +16,4 @@ sudo ansible-pull \
     -i localhost, \
     main.yml && \
 
-echo "It has been done ( ͡° ͜ʖ ͡°)"
+echo -e "\nIt has been done ( ͡° ͜ʖ ͡°)"
